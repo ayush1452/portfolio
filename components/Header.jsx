5964169d543df0368f78214bfaa34670c5f8b7ae
Ayush <ayushsharma1452@gmail.com>
2024-08-10 02:59:34 -0400

    updated the header file

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,8 @@ import { Button } from "./ui/button";
 const Header = () => {
   return (
       <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto" >
+           {/* logo */}
             <Link href="/">
               <h1 className="text-4xl font-semibold">
                 Ayush <span className="text-accent">.</span>
